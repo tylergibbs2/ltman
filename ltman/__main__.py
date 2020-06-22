92f060bcd42f0b57a95888c84381ddc5d822e1e5
Tyler Gibbs <gibbstyler7@gmail.com>
2020-06-22 03:32:12 -0500

    accept text file input

--- ltman/__main__.py
+++ ltman/__main__.py
@@ -10,4 +10,11 @@ parser.add_argument("links", type=str, nargs="+", help="Links to process.")
 args = parser.parse_args()
 
 if __name__ == "__main__":
-    process_all(args.links)
+    to_process = []
+    for link in args.links:
+        if link.endswith("txt"):
+            with open(link, "r") as f:
+                to_process += f.readlines()
+        else:
+            to_process.append(link)
+    process_all(to_process)
